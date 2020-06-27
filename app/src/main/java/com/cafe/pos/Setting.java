36787adaf04c67c76518d4cd19b53e49b7876c7a
anggita97 <anggitakyu@gmail.com>
2020-06-27 10:53:06 +0700

    ghjk

--- app/src/main/java/com/cafe/pos/Setting.java
+++ app/src/main/java/com/cafe/pos/Setting.java
@@ -2,7 +2,9 @@ package com.cafe.pos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Setting extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class Setting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
     }
+    public void profil(View view) {
+        Intent i = new Intent(Setting.this, Profil.class);
+        startActivity(i);
+    }
+    public void pass(View view) {
+        Intent i = new Intent(Setting.this, UbahPassword.class);
+        startActivity(i);
+    }
+    public void logout(View view) {
+        Intent i = new Intent(Setting.this, Login.class);
+        startActivity(i);
+    }
 }
+
