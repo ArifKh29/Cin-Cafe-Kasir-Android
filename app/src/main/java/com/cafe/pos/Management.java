859d78e9cbc4e87de6bf3830e38ed4dfd673cd4c
Arif <arif@gmail.com>
2020-06-27 12:35:20 +0700

    no message

--- app/src/main/java/com/cafe/pos/Management.java
+++ app/src/main/java/com/cafe/pos/Management.java
@@ -2,7 +2,10 @@ package com.cafe.pos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Management extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Management extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management);
+
+    }
+
+    public void tambahMenu(View view) {
+        Dialog dialog = new Dialog(Management.this);
+        dialog.setContentView(R.layout.dialog_layout);
+        dialog.show();
+
+    }
+
+    public void tambahMakanan(View view) {
+        Intent intent = new Intent(Management.this,AddMenu.class);
+        startActivity(intent);
     }
 }
